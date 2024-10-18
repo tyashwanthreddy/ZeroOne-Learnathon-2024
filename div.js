e514f89926f662ba436cf308139ce9261a40dac6
tyashwanthreddy <yash.reddy.0204@gmail.com>
2024-10-18 12:55:00 +0530

    Add files via upload

---
+++ div.js
@@ -0,0 +1,21 @@
+const add =(a,b)=>
+{
+    return a+b;
+}
+console.log(add(2,3));
+const mul =(a,b)=>
+{
+    return a*b;
+}
+console.log(mul(4,5));
+const sub =(a,b)=>
+{
+    return a-b;
+}
+console.log(sub(10,2));
+const div =(a,b)=>
+    {
+        return a/b;
+    }
+    console.log(div(10,2));
+    
---
+++ hello.js
@@ -0,0 +1,3 @@
+let arr =[1,2,3,4,5];
+let newarr=arr.map((x)=> x*x);
+ 
